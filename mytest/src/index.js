8476d3ffa4049a70d2dcfd6d86538bdc939cb9b3
samiksha-3010 <samiksha30102@gmail.com>
2024-01-10 11:47:19 +0530

    done

--- mytest/src/index.js
+++ mytest/src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import AuthContext from "./Component/Context/Au"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthContext>
     <App />
+    </AuthContext>
     </BrowserRouter>
 
   </React.StrictMode>
